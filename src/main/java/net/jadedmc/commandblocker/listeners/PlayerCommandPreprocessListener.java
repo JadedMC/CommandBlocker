e0cc4ef8314386712491c595ebc0612d9d38eda5
colby <colbykrawczyk10@gmail.com>
2023-04-14 22:05:33 -0400

    Switch to Java 8 for compatibility

--- src/main/java/net/jadedmc/commandblocker/listeners/PlayerCommandPreprocessListener.java
+++ src/main/java/net/jadedmc/commandblocker/listeners/PlayerCommandPreprocessListener.java
@@ -16,27 +16,28 @@ public class PlayerCommandPreprocessListener implements Listener {
     public void onCommandSend(PlayerCommandPreprocessEvent event) {
         String mode = plugin.getSettingsManager().getConfig().getString("Mode");
 
+        if(mode == null) {
+            return;
+        }
+
         String command = event.getMessage().split(" ")[0];
 
-        switch (mode.toLowerCase()) {
-            case "whitelist" -> {
-
-                for(String whitelist : plugin.getSettingsManager().getConfig().getStringList("Commands")) {
-                    if(command.equalsIgnoreCase(whitelist)) {
-                        continue;
-                    }
-
+        if(mode.equalsIgnoreCase("blacklist")) {
+            for(String whitelist : plugin.getSettingsManager().getConfig().getStringList("Commands")) {
+                if(command.equalsIgnoreCase(whitelist)) {
                     event.setCancelled(true);
                     break;
                 }
             }
-            case "blacklist" -> {
-                for(String whitelist : plugin.getSettingsManager().getConfig().getStringList("Commands")) {
-                    if(command.equalsIgnoreCase(whitelist)) {
-                        event.setCancelled(true);
-                        break;
-                    }
+        }
+        else if (mode.equalsIgnoreCase("whitelist")) {
+            for(String whitelist : plugin.getSettingsManager().getConfig().getStringList("Commands")) {
+                if(command.equalsIgnoreCase(whitelist)) {
+                    continue;
                 }
+
+                event.setCancelled(true);
+                break;
             }
         }
     }
