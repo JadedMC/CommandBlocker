067c56134daacfb010d40c9b3c30e3c99db15fcf
colby <colbykrawczyk10@gmail.com>
2023-04-20 16:19:24 -0400

    Adds the /cb mode command, which lets the player change the currently plugin mode.

--- src/main/java/net/jadedmc/commandblocker/commands/CommandBlockerCMD.java
+++ src/main/java/net/jadedmc/commandblocker/commands/CommandBlockerCMD.java
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -48,48 +49,79 @@ public class CommandBlockerCMD implements CommandExecutor {
         }
 
         // Get the sub command used.
-        String subCommand = args[0];
+        String subCommand = args[0].toLowerCase();
 
-        // Reloads all plugin configuration files.
-        if(subCommand.equalsIgnoreCase("reload")) {
-            plugin.getSettingsManager().reload();
-            ChatUtils.chat(sender, "<green><bold>CommandBlocker</bold> <dark_gray>» <green>Configuration files reloaded successfully!");
-        }
+        switch (subCommand) {
+            // Adds a command to the command list.
+            case "add":
+                // Makes sure the command is being used properly.
+                if(args.length != 2) {
+                    ChatUtils.chat(sender, "<red><bold>Usage</bold> <dark_gray>» <red>/cb add [command]");
+                    return true;
+                }
 
-        // Displays the plugin's current version.
-        else if (subCommand.equalsIgnoreCase("version")) {
-            ChatUtils.chat(sender, "<green><bold>CommandBlocker</bold> <dark_gray>» <green>Current version: <white>" + plugin.getDescription().getVersion());
-        }
+                // Gets the command being added to the list.
+                String toBlock = args[1];
 
-        else if(subCommand.equalsIgnoreCase("add")) {
-            if(args.length != 2) {
-                ChatUtils.chat(sender, "<red><bold>Usage</bold> <dark_gray>» <red>/cb add [command]");
+                // Makes sure the command starts with a '/'.
+                if(!toBlock.startsWith("/")) {
+                    toBlock = "/" + toBlock;
+                }
+
+                // Adds the command to the list.
+                List<String> commands = plugin.getSettingsManager().getConfig().getStringList("Commands");
+                commands.add(toBlock.toLowerCase());
+
+                // Reloads the config file.
+                plugin.getSettingsManager().getConfig().set("Commands", commands);
+                plugin.getSettingsManager().save();
+                plugin.getSettingsManager().reload();
+
+                ChatUtils.chat(sender, "<green><bold>CommandBlocker</bold> <dark_gray>» <green>Added <white>" + toBlock.toLowerCase() + " <green>to the command list.");
                 return true;
-            }
 
-            String toBlock = args[1];
+            case "mode":
+                // Makes sure the command is being used properly.
+                if(args.length != 2) {
+                    ChatUtils.chat(sender, "<red><bold>Usage</bold> <dark_gray>» <red>/cb mode [mode]");
+                    return true;
+                }
 
-            if(!toBlock.startsWith("/")) {
-                toBlock = "/" + toBlock;
-            }
+                // Gets the intended mode to switch to.
+                String mode = args[1].toUpperCase();
+                List<String> validModes = Arrays.asList("BLACKLIST", "WHITELIST", "HIDE");
 
-            List<String> commands = plugin.getSettingsManager().getConfig().getStringList("Commands");
-            commands.add(toBlock.toLowerCase());
+                // Makes sure the mode is valid.
+                if(!validModes.contains(mode)) {
+                    ChatUtils.chat(sender, "<red><bold>Error</bold> <dark_gray>» <red>Mode must be either <white><hover:show_text:\"Click to select BLACKLIST\"><click:suggest_command:\"/cb mode BLACKLIST\">BLACKLIST</click></hover><red>, <white><hover:show_text:\"Click to select WHITELIST\"><click:suggest_command:\"/cb mode WHITELIST\">WHITELIST</click></hover><red>, or <white><hover:show_text:\"Click to select HIDE\"><click:suggest_command:\"/cb mode HIDE\">HIDE</click></hover><red>.");
+                    return true;
+                }
 
-            plugin.getSettingsManager().getConfig().set("Commands", commands);
-            plugin.getSettingsManager().save();
-            plugin.getSettingsManager().reload();
+                // Reloads the config file.
+                plugin.getSettingsManager().getConfig().set("Mode", mode);
+                plugin.getSettingsManager().save();
+                plugin.getSettingsManager().reload();
 
-            ChatUtils.chat(sender, "<green><bold>CommandBlocker</bold> <dark_gray>» <green>Added <white>" + toBlock.toLowerCase() + " <green>to the command list.");
+                ChatUtils.chat(sender, "<green><bold>CommandBlocker</bold> <dark_gray>» <green>Changed the current mode to <white>" + mode + " <green>.");
+                return true;
+
+            // Reloads all plugin configuration files.
+            case "reload":
+                plugin.getSettingsManager().reload();
+                ChatUtils.chat(sender, "<green><bold>CommandBlocker</bold> <dark_gray>» <green>Configuration files reloaded successfully!");
+                return true;
+
+            // Displays the plugin version.
+            case "version":
+                ChatUtils.chat(sender, "<green><bold>CommandBlocker</bold> <dark_gray>» <green>Current version: <white>" + plugin.getDescription().getVersion());
+                return true;
+
+            // Displays the help menu.
+            default:
+                ChatUtils.chat(sender, "<green><bold>CommandBlocker Commands");
+                ChatUtils.chat(sender, "<green>/cb reload <dark_gray>» <white>Reloads all configuration files.");
+                ChatUtils.chat(sender, "<green>/cb version <dark_gray>» <white>Displays the plugin version.");
+                return true;
         }
-
-        // Displays the help menu.
-        else {
-            ChatUtils.chat(sender, "<green><bold>CommandBlocker Commands");
-            ChatUtils.chat(sender, "<green>/cb reload <dark_gray>» <white>Reloads all configuration files.");
-            ChatUtils.chat(sender, "<green>/cb version <dark_gray>» <white>Displays the plugin version.");
-        }
-
-        return true;
     }
 }
