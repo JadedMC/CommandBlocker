29c7f8e456ad196d5215e92e241a609a0aec2542
colby <colbykrawczyk10@gmail.com>
2023-06-27 15:17:29 -0400

    Improve legacy to MiniMessage translation

--- src/main/java/net/jadedmc/commandblocker/utils/ChatUtils.java
+++ src/main/java/net/jadedmc/commandblocker/utils/ChatUtils.java
@@ -3,7 +3,11 @@ package net.jadedmc.commandblocker.utils;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ChatUtils {
     private static BukkitAudiences adventure;
@@ -36,22 +40,40 @@ public class ChatUtils {
      * @return Message with the color codes replaced.
      */
     public static String replaceLegacy(String message) {
-        return message.replace("&0", "<black>")
-                .replace("&1", "<dark_blue>")
-                .replace("&2", "<dark_green>")
-                .replace("&3", "<dark_aqua>")
-                .replace("&4", "<dark_red>")
-                .replace("&5", "<dark_purple>")
-                .replace("&6", "<gold>")
-                .replace("&7", "<gray>")
-                .replace("&8", "<dark_gray>")
-                .replace("&9", "<blue>")
-                .replace("&a", "<green>")
-                .replace("&b", "<aqua>")
-                .replace("&c", "<red>")
-                .replace("&d", "<light_purple>")
-                .replace("&e", "<yellow>")
-                .replace("&f", "<white>")
+        // Get the server version.
+        String version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+        int subVersion = Integer.parseInt(version.replace("1_", "").replaceAll("_R\\d", "").replace("v", ""));
+
+        // If the version is 1.16 or greater, check for hex color codes.
+        if(subVersion >= 16) {
+            Pattern pattern = Pattern.compile("&#[a-fA-F0-9]{6}");
+            Matcher matcher = pattern.matcher(message);
+
+            while (matcher.find()) {
+                String color = message.substring(matcher.start() + 1, matcher.end());
+                message = message.replace("&" + color, "<reset><color:" + color + ">");
+                matcher = pattern.matcher(message);
+            }
+        }
+
+        // Then replace legacy color codes.
+        return message.replace("§", "&")
+                .replace("&0", "<reset><black>")
+                .replace("&1", "<reset><dark_blue>")
+                .replace("&2", "<reset><dark_green>")
+                .replace("&3", "<reset><dark_aqua>")
+                .replace("&4", "<reset><dark_red>")
+                .replace("&5", "<reset><dark_purple>")
+                .replace("&6", "<reset><gold>")
+                .replace("&7", "<reset><gray>")
+                .replace("&8", "<reset><dark_gray>")
+                .replace("&9", "<reset><blue>")
+                .replace("&a", "<reset><green>")
+                .replace("&b", "<reset><aqua>")
+                .replace("&c", "<reset><red>")
+                .replace("&d", "<reset><light_purple>")
+                .replace("&e", "<reset><yellow>")
+                .replace("&f", "<reset><white>")
                 .replace("&k", "<obfuscated>")
                 .replace("&l", "<bold>")
                 .replace("&m", "<strikethrough>")
