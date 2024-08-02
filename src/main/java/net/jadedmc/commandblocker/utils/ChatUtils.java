abcd56069166656692cdc1863fc6e244d14db3ed
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2024-08-02 01:43:05 -0400

    Allow using proper multi-line YAML Strings in config.yml

--- src/main/java/net/jadedmc/commandblocker/utils/ChatUtils.java
+++ src/main/java/net/jadedmc/commandblocker/utils/ChatUtils.java
@@ -1,17 +1,45 @@
+/*
+ * This file is part of CommandBlocker, licensed under the MIT License.
+ *
+ *  Copyright (c) JadedMC
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 package net.jadedmc.commandblocker.utils;
 
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Some methods to make sending chat messages easier.
+ */
 public class ChatUtils {
     private static BukkitAudiences adventure;
 
-    public static void setAdventure(BukkitAudiences adv) {
+    public static void setAdventure(@NotNull final BukkitAudiences adv) {
         adventure = adv;
     }
 
@@ -20,7 +48,7 @@ public class ChatUtils {
      * @param sender CommandSender to send message to.
      * @param message The message being sent.
      */
-    public static void chat(CommandSender sender, String message) {
+    public static void chat(@NotNull final CommandSender sender, @NotNull final String message) {
         adventure.sender(sender).sendMessage(translate(message));
     }
 
@@ -29,7 +57,7 @@ public class ChatUtils {
      * @param message Message to translate.
      * @return Translated Message.
      */
-    public static Component translate(String message) {
+    public static Component translate(@NotNull final String message) {
         return MiniMessage.miniMessage().deserialize(replaceLegacy(message));
     }
 
@@ -38,12 +66,9 @@ public class ChatUtils {
      * @param message Message to replace color codes in.
      * @return Message with the color codes replaced.
      */
-    public static String replaceLegacy(String message) {
-        // Get the server version.
-        int subVersion = VersionUtils.getServerVersion();
-
+    public static String replaceLegacy(@NotNull String message) {
         // If the version is 1.16 or greater, check for hex color codes.
-        if(subVersion >= 16) {
+        if(VersionUtils.getServerVersion() >= 16) {
             Pattern pattern = Pattern.compile("&#[a-fA-F0-9]{6}");
             Matcher matcher = pattern.matcher(message);
 
@@ -55,7 +80,9 @@ public class ChatUtils {
         }
 
         // Then replace legacy color codes.
-        return message.replace("§", "&")
+        return message
+                .replace("\\n", "<newline>")
+                .replace("§", "&")
                 .replace("&0", "<reset><black>")
                 .replace("&1", "<reset><dark_blue>")
                 .replace("&2", "<reset><dark_green>")
