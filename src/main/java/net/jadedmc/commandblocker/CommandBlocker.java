5fcef5de8f41ba9f064b8b99eb6dfabd48db9a35
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2024-08-01 22:29:01 -0400

    Shade bStats through Maven

--- src/main/java/net/jadedmc/commandblocker/CommandBlocker.java
+++ src/main/java/net/jadedmc/commandblocker/CommandBlocker.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of CommandBlocker, licensed under the MIT License.
+ *
+ *  Copyright (c) JadedMC
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 package net.jadedmc.commandblocker;
 
 import net.jadedmc.commandblocker.commands.CommandBlockerCMD;
@@ -6,6 +30,7 @@ import net.jadedmc.commandblocker.listeners.PlayerCommandSendListener;
 import net.jadedmc.commandblocker.utils.ChatUtils;
 import net.jadedmc.commandblocker.utils.VersionUtils;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CommandBlocker extends JavaPlugin {
