435605e1bef66a2c92ee5ec32d7f032dcc2c0170
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2024-08-01 22:14:25 -0400

    Cache version calculation for *slight* performance improvement

--- src/main/java/net/jadedmc/commandblocker/utils/VersionUtils.java
+++ src/main/java/net/jadedmc/commandblocker/utils/VersionUtils.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of CommandBlocker, licensed under the MIT License.
+ *
+ *  Copyright (c) JadedMC
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 package net.jadedmc.commandblocker.utils;
 
 import org.bukkit.Bukkit;
@@ -6,6 +30,7 @@ import org.bukkit.Bukkit;
  * A collection of utilities related to the version of the server.
  */
 public class VersionUtils {
+    private static int version = -1;
 
     /**
      * Return the major Minecraft version the server is running.
@@ -13,7 +38,14 @@ public class VersionUtils {
      * @return Server's major version.
      */
     public static int getServerVersion() {
-        String version = Bukkit.getServer().getBukkitVersion().split("-")[0];
-        return Integer.parseInt(version.split("\\.")[1]);
+        // Prevent calculating the server version repeatedly.
+        if(version != -1) {
+            return version;
+        }
+
+        // Sets the server version if not set yet.
+        final String versionString = Bukkit.getServer().getBukkitVersion().split("-")[0];
+        version = Integer.parseInt(versionString.split("\\.")[1]);
+        return version;
     }
 }
